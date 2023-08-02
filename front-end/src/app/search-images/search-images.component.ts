9e4e043ffc98f8a9b13e6a8879b12f0fd56031aa
thusitha <nuwan12403482@gmail.com>
2023-08-02 23:25:24 +0530

    Implemented async method to get images list from FlickrService

--- front-end/src/app/search-images/search-images.component.ts
+++ front-end/src/app/search-images/search-images.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import {FlickrService} from "../services/flickr.service";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-images',
@@ -9,4 +11,18 @@ import {faSearch} from "@fortawesome/free-solid-svg-icons";
 export class SearchImagesComponent {
 
   protected readonly faSearch = faSearch;
+  images: any = [];
+  keyword: string = "";
+
+  constructor(private flickrService: FlickrService) {
+  }
+
+  async search(event: any) {
+    this.keyword = event.target.value.toLowerCase();
+    if (this.keyword && this.keyword.length > 0) {
+      const res =  await this.flickrService.search_keyword(this.keyword);
+      this.images = res;
+    }
+
+  }
 }
