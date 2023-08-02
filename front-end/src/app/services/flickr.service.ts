d785c37459c346c36e975ae41d353a77d566b66e
thusitha <nuwan12403482@gmail.com>
2023-08-02 17:08:23 +0530

    Injected HttpClient & Created method to search using keyword

--- front-end/src/app/services/flickr.service.ts
+++ front-end/src/app/services/flickr.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlickrService {
+  prevKeyword: string = "";
+  currPage = 1;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  search_keyword(keyword : string){
+    if (this.prevKeyword === keyword){
+      this.currPage++;
+    } else {
+      this.currPage = 1;
+    }
+    this.prevKeyword = keyword;
+
+  }
 }
