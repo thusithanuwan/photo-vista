4bbbfc815be86c182c80184e132fdd3242c09d38
thusitha <nuwan12403482@gmail.com>
2023-08-02 15:47:17 +0530

    Generated new component for search image & generated service for flickr api

---
+++ front-end/src/app/search-images/search-images.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-images',
+  templateUrl: './search-images.component.html',
+  styleUrls: ['./search-images.component.scss']
+})
+export class SearchImagesComponent {
+
+}
