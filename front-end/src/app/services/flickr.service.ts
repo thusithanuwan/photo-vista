383ae94fabb3863bc34fc33e406cbcf0e4716df3
thusitha <nuwan12403482@gmail.com>
2023-08-02 20:48:11 +0530

    Created GET request that return photoObj array

--- front-end/src/app/services/flickr.service.ts
+++ front-end/src/app/services/flickr.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
+import {map} from "rxjs";
+import * as stream from "stream";
 
 
-
-export interface FlickrPhoto{
+export interface FlickrPhoto {
   farm: string;
   id: string;
   secret: string;
@@ -26,10 +27,11 @@ export class FlickrService {
   prevKeyword: string = "";
   currPage = 1;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  search_keyword(keyword : string){
-    if (this.prevKeyword === keyword){
+  search_keyword(keyword : string) {
+    if (this.prevKeyword === keyword) {
       this.currPage++;
     } else {
       this.currPage = 1;
@@ -38,8 +40,19 @@ export class FlickrService {
     const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
     const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=12&page=${this.currPage}`
 
-
-
-
+    return this.http.get<FlickrOutput>(url + params).pipe(map((res: FlickrOutput) => {
+      const urlArr : {url : string ; title : string} [] = [];
+      res.photos.photo.forEach((ph: FlickrPhoto) => {
+        const photoObj = {
+          url: `https://farm${ph.farm}.staticflickr.com/${ph.server}/${ph.id}_${ph.secret}`,
+          title: ph.title
+        };
+        urlArr.push(photoObj);
+      });
+      return urlArr;
+    }));
   }
+
+
 }
+
