12a51a313d99fb30b667a3864f3ed20f2bb20290
thusitha <nuwan12403482@gmail.com>
2023-08-02 18:32:29 +0530

    Created two interfaces to define the structure of objects

--- front-end/src/app/services/flickr.service.ts
+++ front-end/src/app/services/flickr.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+
+
+export interface FlickrPhoto{
+  farm: string;
+  id: string;
+  secret: string;
+  server: string;
+  title: string;
+}
+
+export interface FlickrOutput {
+  photos: {
+    photo: FlickrPhoto[];
+  };
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class FlickrService {
   prevKeyword: string = "";
   currPage = 1;
@@ -17,6 +35,11 @@ export class FlickrService {
       this.currPage = 1;
     }
     this.prevKeyword = keyword;
+    const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&';
+    const params = `api_key=${environment.flickr.key}&text=${keyword}&format=json&nojsoncallback=1&per_page=12&page=${this.currPage}`
+
+
+
 
   }
 }
