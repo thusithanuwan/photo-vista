d63575b83e39800223cce883d75becfb32ef47df
thusitha <nuwan12403482@gmail.com>
2023-08-04 02:20:40 +0530

    moved css to styles.scss & gave id to dwn btn

--- front-end/src/app/gallery/gallery.component.ts
+++ front-end/src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment.development";
+import {faDownload} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-gallery',
@@ -45,4 +46,6 @@ export class GalleryComponent {
     });
 
   }
+
+  protected readonly faDownload = faDownload;
 }
