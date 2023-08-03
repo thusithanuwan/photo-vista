70aae2cc4fe8ae94a5c5954ef9ead07e71c727b6
thusitha <nuwan12403482@gmail.com>
2023-08-04 02:10:47 +0530

    Created GET & POST request to load and save images

--- front-end/src/app/gallery/gallery.component.ts
+++ front-end/src/app/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment.development";
 
 @Component({
   selector: 'app-gallery',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
+    show = false;
+    imageList:any = [];
 
+    constructor(private http : HttpClient) {
+      this.http.get(environment.REST_API_URL).subscribe((imageUrlList)=> {
+        this.imageList = imageUrlList;
+      });
+    }
+
+  preventDefault(event: DragEvent) {
+    event.preventDefault();
+
+
+  }
+
+  drop(event: DragEvent) {
+    event.preventDefault();
+    const droppedFiles = event.dataTransfer?.files;
+    if (!droppedFiles) return;
+    const imageFiles = Array.from(droppedFiles)
+      .filter(file => file.type.startsWith("image/"));
+    if (!imageFiles.length) return;
+    this.uploadImages(imageFiles);
+  }
+
+  uploadImages(imageFiles:any){
+    const formData = new FormData();
+    imageFiles.forEach((image : File) => {
+      formData.append("images", image);
+    });
+
+    this.http.post(environment.REST_API_URL, formData).subscribe((imageUrl)=>{
+      this.imageList.append(imageUrl)
+      this.show = true;
+    });
+
+  }
 }
