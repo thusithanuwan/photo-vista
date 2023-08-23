65482349e6fc098036aa4c71c24174944de3b227
thusitha <nuwan12403482@gmail.com>
2023-08-23 21:13:27 +0530

    Changed REST_API_URL

--- front-end/src/environments/environment.development.ts
+++ front-end/src/environments/environment.development.ts
@@ -4,5 +4,5 @@ export const environment = {
   flickr: {
     key: '9298e4c547733692da1e20eac35ce0af'
   },
-  REST_API_URL : `http://localhost:8080/gallery/images`
+  REST_API_URL : `http://localhost:8080/gallery/api/v1/images`
 };
